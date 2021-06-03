8006d39c4f12d410cca15451e7b95417670ef55e
lovanto <rifkylovanto@gmail.com>
2021-06-03 16:00:29 +0700

    Disukai.cs

---
+++ FlashLaundry/Disukai.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Disukai
+{
+    // Nama Class Disukai
+    public class Disukai
+    {
+        // Deklarasi variabel id jasa & id pengguna
+        private String _idJasa, _idPengguna;
+
+        // Mengambil nilai id jasa
+        public String getIdJasa()
+        {
+            return _idJasa;
+        }
+
+        // Mengeset nilai id jasa
+        public void setIdJasa(String idJasa)
+        {
+            this._idJasa = idJasa;
+        }
+
+        // Mengambil nilai id Pengguna
+        public String getIdPengguna()
+        {
+            return _idPengguna;
+        }
+
+        // Mengeset nilai id Pengguna
+        public void setIdPengguna(String idPengguna)
+        {
+            this._idPengguna = idPengguna;
+        }
+
+        //Mengisi data variabel dengan data yang dibutuhkan (constructor)
+        public Disukai(String idJasa, String idPengguna)
+        {
+            this._idJasa = idJasa;
+            this._idPengguna = idPengguna;
+        }
+    }
+}
